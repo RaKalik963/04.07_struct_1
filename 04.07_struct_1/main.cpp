0aca27bd050a5f75891566815cf8577ac86dc23b
RaKalik963 <andrejmahot646@gmail.com>
2024-07-04 20:46:32 +0300

    324324

--- 04.07_struct_1/main.cpp
+++ 04.07_struct_1/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
-
+#include <Windows.H>
 using namespace std;
 
 struct Box
 {
-private:
-	int* _width;
-	int* _height;
-public:
-	Box()
-	{
-		_width = nullptr;
-		_height = nullptr;
-	}
-	
+	int width;
+	int height;
+	int x;
+	int y;
 };
-
-
-
-
-int main()
+void change_size(Box& rectangle, int newWidth, int newHeight)
 {
-	Box  = {};
+	rectangle.width = newWidth;
+	rectangle.height = newHeight;
+}
+void move_rect(Box& rectangle, int x, int y)
+{
+	rectangle.x = x;
+	rectangle.y = y;
+}
+void print_rectangle(Box& rectangle)
+{
+	int x
+}
+
+
+
+int main() {
+	Box rectangle;
+	rectangle.width = 10;
+	rectangle.height = 5;
+	rectangle.x = 3;
+	rectangle.y = 2;
+
 	system("pause");
 	return 0;
 }
